7bc115329857b18605e886e2294b41f384979548
113 <113@192.168.219.102>
2021-05-29 00:39:39 +0900

    05291239

---
+++ miniProject/src/com/kh/tm1/controller/asdasd11.java
@@ -0,0 +1,6 @@
+package com.kh.tm1.controller;
+
+public class asdasd11 {
+	private int a;
+	private int b;
+}
--- miniProject/src/com/kh/tm1/run/Run.java
+++ miniProject/src/com/kh/tm1/run/Run.java
@@ -1,11 +1,16 @@
 package com.kh.tm1.run;
 
+import com.kh.tm1.controller.asdasd11;
+
 public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-g enerated method stub
 		System.out.println("TEST");
 		System.out.println("TEST!!!");
+		
+		asdasd11 a = new asdasd11();
+		System.out.println("ぞしぞし");
 
 	}
 
