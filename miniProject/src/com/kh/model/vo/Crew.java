56431e326b542ef38bb81590a6d5c51354fb025d
kthss01 <kthss01@naver.com>
2021-06-04 15:29:12 +0900

    크루 모델 수정 크루 내용 필드 추가

--- miniProject/src/com/kh/model/vo/Crew.java
+++ miniProject/src/com/kh/model/vo/Crew.java
@@ -11,16 +11,18 @@ public class Crew implements Serializable {
 //	private User crewMaster; // 크루장 
 	private String crewMasterName; // 크루장 일단 String으로처리 (크루장은 User이름)
 	private ArrayList<User> userList; // 크루원 리스트
+	private String crewContents; // 크루 내용
 	private boolean isAccept; // 크루 만들기 승인 여부 관리자가 승인 기본값 false
 
 	public Crew() {
 		userList = new ArrayList<User>();
 	}
 
-	public Crew(String crewName, String crewMasterName) {
+	public Crew(String crewName, String crewMasterName, String crewContents) {
 		this();
 		this.crewName = crewName;
 		this.crewMasterName = crewMasterName;
+		this.crewContents = crewContents;
 //		this.isAccept = false; // 기본값은 승인이 안된 상태 
 	}
 
@@ -40,6 +42,14 @@ public class Crew implements Serializable {
 		this.crewMasterName = crewMasterName;
 	}
 
+	public String getCrewContents() {
+		return crewContents;
+	}
+	
+	public void setCrewContents(String crewContents) {
+		this.crewContents = crewContents;
+	}
+	
 	public boolean isAccept() {
 		return isAccept;
 	}
