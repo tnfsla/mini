ec34dd09998939c26f2fc48eabee81e765b759b2
kthss01 <kthss01@naver.com>
2021-06-04 18:51:53 +0900

    Crew Part Frame -> Panel 변경 프레임 전환시 문제로 인해 패널 변경으로 전체 수정

--- miniProject/src/com/kh/view/crew/CrewRankPanel.java
+++ miniProject/src/com/kh/view/crew/CrewRankPanel.java
@@ -1,54 +1,45 @@
 package com.kh.view.crew;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
+import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import com.kh.controller.crew.CrewRankController;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
-public class CrewRankFrame extends JFrame {
+public class CrewRankPanel extends JPanel {
 
 	private CrewViewManager crewManager;
-	
+
 	private CrewRankController rankController;
 
-	private JPanel contentPane;
-
-	public CrewRankFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 360, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+	public CrewRankPanel() {
+		setBounds(0, 0, 360, 600);
+		setLayout(null);
 
 		JButton btnNewButton = new JButton("뒤로가기");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// 특정 crew 페이지로 다시 이동
-				crewManager.convertFrame("crew");
+				crewManager.convertPanel("crew");
 			}
 		});
 		btnNewButton.setBounds(12, 10, 97, 23);
-		contentPane.add(btnNewButton);
+		add(btnNewButton);
 
 		JButton btnNewButton_1 = new JButton("홈");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				crewManager.convertFrame("main"); // main 페이지로
+				crewManager.convertPanel("main"); // main 페이지로
 			}
 		});
 		btnNewButton_1.setBounds(125, 528, 97, 23);
-		contentPane.add(btnNewButton_1);
+		add(btnNewButton_1);
 	}
 
-	public CrewRankFrame(CrewViewManager crewManager, CrewRankController rankController) {
+	public CrewRankPanel(CrewViewManager crewManager, CrewRankController rankController) {
 		this();
 		this.crewManager = crewManager;
 		this.rankController = rankController;
@@ -58,8 +49,8 @@ public class CrewRankFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					CrewRankFrame frame = new CrewRankFrame();
-					frame.setVisible(true);
+					CrewRankPanel panel = new CrewRankPanel();
+					panel.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
