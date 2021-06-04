14a56af9269c737b4ec19f72441f3f25b9242258
yoo <yoo@DESKTOP-Q9R85KE>
2021-06-04 20:45:43 +0900

    운동기록확인

--- miniProject/src/com/kh/run/Run.java
+++ miniProject/src/com/kh/run/Run.java
@@ -1,13 +1,13 @@
 package com.kh.run;
 
-import com.kh.calendar.CalendarMain;
+import com.kh.view.result.ResultMainView;
 
 public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		new CalendarMain().CalendarMain();
+		new ResultMainView().CalendarMain();
 	}
 
 }
