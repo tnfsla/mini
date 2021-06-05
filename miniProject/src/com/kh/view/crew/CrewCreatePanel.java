3200b52736e9d745ad39dbf9bb9525387f342f90
kthss01 <kthss01@naver.com>
2021-06-06 01:04:44 +0900

    Crew Part CrewCreatePanel, CrewCreate 구현 크루 만들기 시 문제 되는 부분을 파악 크루는 현재 객체 입출력으로 크루 정보들을 읽어오는데 이 경우 크루원 (User)또한 읽어오게된다. 여기서 문제는 로그인/회원가입을 통해 접속(new, 객체생성)한 User와 크루 가입시에 크루 내에 User와 같은 User이지만 객체는 2개가 된다는 것이다. 이것을 해결하기 위해 CrewDao에서 UpdateUser 메소드를 만들어 User가 크루에 가입한 상태이면 User를 찾아서 객체를 바꿔주도록 하였다. DB를 쓰지 않고 파일 입출력으로 해서 발생하는 문제라고 본다. 다행히 크루 부분을 제외하고는 다른 파트에서는 문제가 발생하지 않을것으로 보인다.

--- miniProject/src/com/kh/view/crew/CrewCreatePanel.java
+++ miniProject/src/com/kh/view/crew/CrewCreatePanel.java
@@ -8,34 +8,90 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import com.kh.controller.crew.CrewCreateController;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.SystemColor;
+import javax.swing.UIManager;
 
 public class CrewCreatePanel extends JPanel {
 
 	private CrewViewManager crewManager;
 	
 	private CrewCreateController createController;
+	
+	private JTextField textFieldCrewName; // 크루명이 입력되는 텍스트필드
+	private JTextArea textAreaCrewContents; // 크루 소개가 입력되는 텍스트 에리어
 
 	public CrewCreatePanel() {
+		setForeground(Color.BLACK);
+		setBackground(Color.WHITE);
 		setBounds(0, 0, 360, 600);
 		setLayout(null);
 
-		JButton btnNewButton = new JButton("완료");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnCreateCrew = new JButton("완료");
+		btnCreateCrew.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				createController.createCrew(textFieldCrewName.getText(), textAreaCrewContents.getText(), crewManager.getUser().getName());
 				crewManager.convertPanel("main"); // main page로
 			}
 		});
-		btnNewButton.setBounds(235, 20, 97, 23);
-		add(btnNewButton);
-
-		JButton btnNewButton_1 = new JButton("홈");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				crewManager.convertPanel("main"); // main page로
+		btnCreateCrew.setBounds(235, 20, 97, 23);
+		add(btnCreateCrew);
+		
+		JPanel panelTextArea = new JPanel();
+		panelTextArea.setBounds(15, 270, 330, 240);
+		add(panelTextArea);
+		panelTextArea.setLayout(null);
+		
+		textAreaCrewContents = new JTextArea();
+		textAreaCrewContents.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				textAreaCrewContents.setText("");
 			}
 		});
-		btnNewButton_1.setBounds(131, 528, 97, 23);
-		add(btnNewButton_1);
+		textAreaCrewContents.setText("내용을 입력해주세요");
+		textAreaCrewContents.setToolTipText("");
+		textAreaCrewContents.setBounds(5, 5, 320, 230);
+		panelTextArea.add(textAreaCrewContents);
+		textAreaCrewContents.setBackground(Color.WHITE);
+		textAreaCrewContents.setToolTipText("");
+		
+		JLabel lblCrewContents = new JLabel("소 개");
+		lblCrewContents.setBounds(12, 228, 79, 23);
+		add(lblCrewContents);
+		
+		JLabel lblCrewName = new JLabel("크루 명");
+		lblCrewName.setBounds(12, 86, 66, 23);
+		add(lblCrewName);
+		
+		JPanel panelTextField = new JPanel();
+		panelTextField.setBounds(22, 119, 310, 38);
+		add(panelTextField);
+		panelTextField.setLayout(null);
+		
+		textFieldCrewName = new JTextField();
+		textFieldCrewName.setBounds(50, 0, 260, 30);
+		panelTextField.add(textFieldCrewName);
+		textFieldCrewName.setColumns(10);
+		
+		JPanel footerPanel = new JPanel();
+		footerPanel.setBackground(Color.LIGHT_GRAY);
+		footerPanel.setBounds(0, 561, 360, 29);
+		add(footerPanel);
+		
+		JLabel lblHome = new JLabel("Home");
+		lblHome.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				crewManager.convertPanel("main");
+			}
+		});
+		footerPanel.add(lblHome);
 	}
 
 	public CrewCreatePanel(CrewViewManager crewManager, CrewCreateController createController) {
