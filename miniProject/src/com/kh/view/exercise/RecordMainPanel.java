0c65ca08bca5d60d7d624f6c9bf140e9c27afd3d
minji <ioplkju1015@naver.com>
2021-06-10 00:53:14 +0900

    충돌...

--- miniProject/src/com/kh/view/exercise/RecordMainPanel.java
+++ miniProject/src/com/kh/view/exercise/RecordMainPanel.java
@@ -1,14 +1,40 @@
 package com.kh.view.exercise;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class RecordMainPanel extends JPanel {
+import com.kh.controller.exercise.RecordMainController;
+import com.kh.model.vo.User;
 
-	/**
-	 * Create the panel.
-	 */
+import java.awt.event.ActionListener;
+import java.util.Map;
+import java.awt.event.ActionEvent;
+
+public class RecordMainPanel extends JPanel {
+	
+private RecordMainController recoMainController;
+private User user;
+	
+	private JPanel mainPanel; //메인 - 시작
+	private RecordMainPanel rmp; // 기록하기메인
+	private InputRecordPanel irp; // 시간, 거리 입력
+	private AutoRecordPanel arp; // 스탑워치로 입력
+	private StopWatchPanel swp; //스탑워치 패널
+	private ExStart es; //시간 기록
+	
+	private Map<String, JPanel> panelMap;
+	
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	
+	private JButton b1; //auto로 연결
+	private JButton b2; //input으로 연결
+	private JButton cf; //input 확인
+	private JButton save; //input 저장
+	private JButton cfA; //auto 확인
+	private JButton saveA; //auto 저장
+	
 	public RecordMainPanel() {
 
 		setBounds(100, 100, 360, 600);
@@ -24,7 +50,7 @@ public class RecordMainPanel extends JPanel {
 		b1.setLocation(80, 250);
 		b1.setSize(180, 80);
 		add(b1);
-
+		
 		JButton b2 = new JButton("자율 운동 기록");
 		b2.setLocation(80, 401);
 		b2.setSize(180, 80);
