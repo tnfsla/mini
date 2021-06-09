d0611addb331ee09076a82c97e88c2b99e91d4a4
minji <ioplkju1015@naver.com>
2021-06-09 19:50:27 +0900

    Auto - 시간측정 후 운동정보 입력하는 패널 exStart - 스탑워치 실행 메소드 Input - 시간, 거리 사용자가 입력하는 패널 stopwatch - 스탑워치 GUI

---
+++ miniProject/src/com/kh/view/exercise/InputRecordPanel.java
@@ -0,0 +1,213 @@
+package com.kh.view.exercise;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+
+public class InputRecordPanel extends JPanel {
+
+	public InputRecordPanel() {
+
+		setBounds(0, 0, 360, 600);
+		setLayout(null);
+
+		// 시간 입력
+
+		JLabel time = new JLabel("시간");
+		time.setHorizontalAlignment(SwingConstants.CENTER);
+		time.setLocation(30, 40);
+		time.setSize(50, 40);
+		add(time);
+
+		JLabel slash1 = new JLabel(":");
+		slash1.setLocation(145, 40);
+		slash1.setSize(10, 40);
+		add(slash1);
+
+		JLabel slash2 = new JLabel(":");
+		slash2.setLocation(225, 40);
+		slash2.setSize(10, 40);
+		add(slash2);
+
+		JTextField h = new JTextField("시간");
+		h.setHorizontalAlignment(SwingConstants.CENTER);
+		JTextField m = new JTextField("분");
+		m.setHorizontalAlignment(SwingConstants.CENTER);
+		JTextField s = new JTextField("초");
+		s.setHorizontalAlignment(SwingConstants.CENTER);
+
+		h.setLocation(80, 40);
+		m.setLocation(160, 40);
+		s.setLocation(240, 40);
+
+		h.setSize(50, 40);
+		m.setSize(50, 40);
+		s.setSize(50, 40);
+
+		add(h);
+		add(m);
+		add(s);
+		
+		h.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			h.setText("");
+			}
+		});
+		m.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			m.setText("");
+			}
+		});
+		s.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			s.setText("");
+			}
+		});
+		
+
+		// 거리 입력
+
+		JLabel km1 = new JLabel("거리");
+		km1.setHorizontalAlignment(SwingConstants.CENTER);
+		km1.setLocation(30, 120);
+		km1.setSize(50, 40);
+		add(km1);
+
+		JTextField km2 = new JTextField("0.0");
+		km2.setHorizontalAlignment(SwingConstants.RIGHT);
+		km2.setLocation(80, 120);
+		km2.setSize(150, 40);
+		add(km2);
+
+		JLabel km3 = new JLabel("km");
+		km3.setHorizontalAlignment(SwingConstants.CENTER);
+		km3.setLocation(250, 120);
+		km3.setSize(50, 40);
+		add(km3);
+		
+		km2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			km2.setText("");
+			}
+		});
+
+		// 페이스 출력
+		JLabel pace1 = new JLabel("달리기 페이스");
+		pace1.setHorizontalAlignment(SwingConstants.CENTER);
+		pace1.setLocation(50, 230);
+		pace1.setSize(100, 30);
+		add(pace1);
+
+		JLabel pacef = new JLabel("00.00");
+		pacef.setHorizontalAlignment(SwingConstants.CENTER);
+		pacef.setLocation(50, 270);
+		pacef.setSize(100, 30);
+		add(pacef);
+
+		// 칼로리
+		JLabel kcal1 = new JLabel("칼로리 소모량");
+		kcal1.setHorizontalAlignment(SwingConstants.CENTER);
+		kcal1.setLocation(200, 230);
+		kcal1.setSize(100, 30);
+		add(kcal1);
+		
+		JLabel kcalf = new JLabel("0 kcal");
+		kcalf.setHorizontalAlignment(SwingConstants.CENTER);
+		kcalf.setLocation(200, 270);
+		kcalf.setSize(100, 30);
+		add(kcalf);
+
+		// 확인
+		JButton cf = new JButton("확인");
+		cf.setLocation(145, 180);
+		cf.setSize(70, 40);
+		add(cf);
+
+		cf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				//사용자가 입력한 시간 거리 가져오기
+				String h1 = h.getText();
+				String m1 = m.getText();
+				String s1 = s.getText();
+				String kmm = km3.getText();
+				
+				//사용자가 입력한 값을 계산하기 위해 형변환
+				int hi = Integer.parseInt(h1);
+				int mi = Integer.parseInt(m1);
+				int si = Integer.parseInt(s1);
+				double ki = Double.valueOf(kmm).doubleValue();
+				
+				
+				double rrp = (((hi * 60) + mi + (si / 60)) / ki); //페이스 계산
+				String fp = String.format("%.2f", rrp); //계산된 페이스값 문자열로 저장
+				
+				int kc =  ((hi * 60) + mi + (si / 60)) * 6; //칼로리계산 (분당 6kcal로리 소모)
+				String kcf = Integer.toString(kc);
+				
+
+				pacef.setText(fp); //페이스값을 보여주기
+				kcalf.setText(kcf + " kcal"); //칼로리값 보여주기
+
+			}
+		});
+
+		// 운동평가
+		JLabel star1 = new JLabel("운동 평가");
+		star1.setLocation(50, 350);
+		star1.setSize(80, 30);
+		add(star1);
+
+		JButton s1 = new JButton("");
+		s1.setIcon(new ImageIcon("C:\\Users\\minji\\Desktop\\별1.png"));
+		s1.setPressedIcon(new ImageIcon("C:\\Users\\minji\\Desktop\\별2.png"));
+		s1.setLocation(35, 390);
+		s1.setSize(60, 60);
+		add(s1);
+		JButton s2 = new JButton("2");
+		s2.setLocation(95, 390);
+		s2.setSize(60, 60);
+		add(s2);
+		JButton s3 = new JButton("3");
+		s3.setLocation(155, 390);
+		s3.setSize(60, 60);
+		add(s3);
+		JButton s4 = new JButton("4");
+		s4.setLocation(215, 390);
+		s4.setSize(60, 60);
+		add(s4);
+		JButton s5 = new JButton("5");
+		s5.setLocation(275, 390);
+		s5.setSize(60, 60);
+		add(s5);
+		
+		s1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		
+
+		// 저장
+		JButton save = new JButton("저장");
+		save.setLocation(145, 480);
+		save.setSize(70, 40);
+		add(save);
+
+		setVisible(true);
+
+	}
+
+}
---
+++ miniProject/src/com/kh/view/exercise/StopWatchPanel.java
@@ -0,0 +1,76 @@
+package com.kh.view.exercise;
+
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class StopWatchPanel extends JPanel {
+
+	JButton startB;
+	JButton stopB;
+	
+	JLabel hour = new JLabel("00", 2);
+	JLabel min = new JLabel("00", 2);
+	JLabel sec = new JLabel("00", 2);
+	JLabel slash1 = new JLabel(":");
+	JLabel slash2 = new JLabel(":");
+	
+	public StopWatchPanel() {
+		
+		setBounds(100, 100, 360, 600);
+		setLayout(null);
+		
+		
+		JTextArea t1 = new JTextArea(
+				"운동 시간 측정을 위해" + "\n" + "운동시작 버튼을 눌러주세요." + "\n" + "운동이 끝난 후에는" + "\n" + "반드시 종료버튼을 눌러 시간을 기록해 주세요.");
+		t1.setLocation(30, 80);
+		t1.setSize(270, 100);
+		add(t1);
+		
+		JLabel hour = new JLabel("00", 2);
+		JLabel min = new JLabel("00", 2);
+		JLabel sec = new JLabel("00", 2);
+		hour.setSize(100, 100);
+		min.setSize(100, 100);
+		sec.setSize(100, 100);
+		hour.setFont(new Font("Agency FB", Font.BOLD, 70));
+		min.setFont(new Font("Agency FB", Font.BOLD, 70));
+		sec.setFont(new Font("Agency FB", Font.BOLD, 70));
+		hour.setLocation(40, 200);
+		min.setLocation(140, 200);
+		sec.setLocation(232, 200);
+		
+		add(hour);
+		add(min);
+		add(sec);
+		
+		JLabel slash1 = new JLabel(":",2);
+		JLabel slash2 = new JLabel(":",2);
+		
+		slash1.setLocation(118, 225);
+		slash1.setSize(20, 40);
+		slash1.setFont(new Font("Agency FB", Font.BOLD, 50));
+
+		slash2.setLocation(213, 225);
+		slash2.setSize(20, 40);
+		slash2.setFont(new Font("Agency FB", Font.BOLD, 50));
+		
+		add(slash1);
+		add(slash2);
+		
+		JButton startB = new JButton("Start");
+		startB.setLocation(110, 350);
+		startB.setSize(120, 50);
+		add(startB);
+		
+		JButton stopB = new JButton("Stop");
+		stopB.setLocation(110, 420);
+		stopB.setSize(120, 50);
+		add(stopB);
+		
+	}
+}
+
