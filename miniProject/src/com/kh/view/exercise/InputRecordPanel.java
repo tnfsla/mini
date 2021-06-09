6673cf0658cd534ab9a16ba3f2b293b8a85ac96e
minji <ioplkju1015@naver.com>
2021-06-09 21:57:05 +0900

    운동평가 부분 수정

--- miniProject/src/com/kh/view/exercise/InputRecordPanel.java
+++ miniProject/src/com/kh/view/exercise/InputRecordPanel.java
@@ -15,6 +15,9 @@ import javax.swing.SwingConstants;
 public class InputRecordPanel extends JPanel {
 
 	public InputRecordPanel() {
+		
+		ImageIcon ss1 = new ImageIcon("./image/star1.png");
+		ImageIcon ss2 = new ImageIcon("./image/star2.png");
 
 		setBounds(0, 0, 360, 600);
 		setLayout(null);
@@ -55,26 +58,25 @@ public class InputRecordPanel extends JPanel {
 		add(h);
 		add(m);
 		add(s);
-		
+
 		h.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-			h.setText("");
+				h.setText("");
 			}
 		});
 		m.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-			m.setText("");
+				m.setText("");
 			}
 		});
 		s.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-			s.setText("");
+				s.setText("");
 			}
 		});
-		
 
 		// 거리 입력
 
@@ -95,11 +97,11 @@ public class InputRecordPanel extends JPanel {
 		km3.setLocation(250, 120);
 		km3.setSize(50, 40);
 		add(km3);
-		
+
 		km2.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-			km2.setText("");
+				km2.setText("");
 			}
 		});
 
@@ -122,7 +124,7 @@ public class InputRecordPanel extends JPanel {
 		kcal1.setLocation(200, 230);
 		kcal1.setSize(100, 30);
 		add(kcal1);
-		
+
 		JLabel kcalf = new JLabel("0 kcal");
 		kcalf.setHorizontalAlignment(SwingConstants.CENTER);
 		kcalf.setLocation(200, 270);
@@ -137,68 +139,112 @@ public class InputRecordPanel extends JPanel {
 
 		cf.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
-				//사용자가 입력한 시간 거리 가져오기
+
+				// 사용자가 입력한 시간 거리 가져오기
 				String h1 = h.getText();
 				String m1 = m.getText();
 				String s1 = s.getText();
 				String kmm = km3.getText();
-				
-				//사용자가 입력한 값을 계산하기 위해 형변환
+
+				// 사용자가 입력한 값을 계산하기 위해 형변환
 				int hi = Integer.parseInt(h1);
 				int mi = Integer.parseInt(m1);
 				int si = Integer.parseInt(s1);
 				double ki = Double.valueOf(kmm).doubleValue();
-				
-				
-				double rrp = (((hi * 60) + mi + (si / 60)) / ki); //페이스 계산
-				String fp = String.format("%.2f", rrp); //계산된 페이스값 문자열로 저장
-				
-				int kc =  ((hi * 60) + mi + (si / 60)) * 6; //칼로리계산 (분당 6kcal로리 소모)
-				String kcf = Integer.toString(kc);
-				
 
-				pacef.setText(fp); //페이스값을 보여주기
-				kcalf.setText(kcf + " kcal"); //칼로리값 보여주기
+				double rrp = (((hi * 60) + mi + (si / 60)) / ki); // 페이스 계산
+				String fp = String.format("%.2f", rrp); // 계산된 페이스값 문자열로 저장
+
+				int kc = ((hi * 60) + mi + (si / 60)) * 6; // 칼로리계산 (분당 6kcal로리 소모)
+				String kcf = Integer.toString(kc);
+
+				pacef.setText(fp); // 페이스값을 보여주기
+				kcalf.setText(kcf + " kcal"); // 칼로리값 보여주기
 
 			}
 		});
 
-		// 운동평가
 		JLabel star1 = new JLabel("운동 평가");
 		star1.setLocation(50, 350);
 		star1.setSize(80, 30);
 		add(star1);
 
-		JButton s1 = new JButton("");
-		s1.setIcon(new ImageIcon("C:\\Users\\minji\\Desktop\\별1.png"));
-		s1.setPressedIcon(new ImageIcon("C:\\Users\\minji\\Desktop\\별2.png"));
+		JButton s1 = new JButton();
+		s1.setIcon(ss1);
 		s1.setLocation(35, 390);
 		s1.setSize(60, 60);
 		add(s1);
-		JButton s2 = new JButton("2");
+
+		JButton s2 = new JButton(ss1);
 		s2.setLocation(95, 390);
 		s2.setSize(60, 60);
 		add(s2);
-		JButton s3 = new JButton("3");
+
+		JButton s3 = new JButton(ss1);
 		s3.setLocation(155, 390);
 		s3.setSize(60, 60);
 		add(s3);
-		JButton s4 = new JButton("4");
+
+		JButton s4 = new JButton(ss1);
 		s4.setLocation(215, 390);
 		s4.setSize(60, 60);
 		add(s4);
-		JButton s5 = new JButton("5");
+		JButton s5 = new JButton(ss1);
 		s5.setLocation(275, 390);
 		s5.setSize(60, 60);
 		add(s5);
-		
-		s1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
+
+		s1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				s1.setIcon(ss2);
+				s2.setIcon(ss1);
+				s3.setIcon(ss1);
+				s4.setIcon(ss1);
+				s5.setIcon(ss1);
 			}
 		});
-		
+		s2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				s1.setIcon(ss2);
+				s2.setIcon(ss2);
+				s3.setIcon(ss1);
+				s4.setIcon(ss1);
+				s5.setIcon(ss1);
+			}
+		});
+		s3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				s1.setIcon(ss2);
+				s2.setIcon(ss2);
+				s3.setIcon(ss2);
+				s4.setIcon(ss1);
+				s5.setIcon(ss1);
+			}
+		});
+		s4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				s1.setIcon(ss2);
+				s2.setIcon(ss2);
+				s3.setIcon(ss2);
+				s4.setIcon(ss2);
+				s5.setIcon(ss1);
+			}
+		});
+		s5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				s1.setIcon(ss2);
+				s2.setIcon(ss2);
+				s3.setIcon(ss2);
+				s4.setIcon(ss2);
+				s5.setIcon(ss2);
+
+			}
+		});
 
 		// 저장
 		JButton save = new JButton("저장");
