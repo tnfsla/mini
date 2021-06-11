863b30ae161771b92a390f7e47facd126224542d
reton <reton@192.168.1.110>
2021-06-11 10:53:38 +0900

    # WARNING: head commit changed in the meantime
    pull request

--- miniProject/src/com/kh/view/login/LoginJoinPanel.java
+++ miniProject/src/com/kh/view/login/LoginJoinPanel.java
@@ -1,8 +1,10 @@
 package com.kh.view.login;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.Panel;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -14,15 +16,22 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-public class LoginJoinPanel extends JPanel {
+import java.awt.BorderLayout;
+import java.awt.Color;
 
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+public class LoginJoinPanel {
+
+	private JFrame frame;
+	private JTextField name;
+	private JTextField id;
+	private JTextField pw;
+	private JTextField age;
+	private JTextField Height;
+	private JTextField Weight;
+	private JTextField txty;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -31,12 +40,8 @@ public class LoginJoinPanel extends JPanel {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					JFrame window = new JFrame();
-					LoginJoinPanel joinPanel = new LoginJoinPanel();
-					window.setBounds(100, 100, 360, 600);
-					window.getContentPane().add(joinPanel);
-					window.setVisible(true);
-					window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					LoginJoinPanel window = new LoginJoinPanel();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -55,91 +60,102 @@ public class LoginJoinPanel extends JPanel {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		setBounds(100, 100, 360, 600);
-		setLayout(null);
+		frame = new JFrame();
+		frame.setTitle("UserJoin");
+		frame.setBounds(100, 100, 360, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		ImagePanel panel = new ImagePanel(new ImageIcon("./image/UserJoinPage.png").getImage());
-		add(panel);
-
+		ImagePanel panel = new ImagePanel(new ImageIcon("./image/LoginJoinPanelImage.jpg").getImage());
+		frame.getContentPane().add(panel);
 		
-		File f = new File("./resources/UserJoinpage.jpg");
-		System.out.println(f.exists()?"Exists":"doesnt exists");// 파일 경로 찾기 방법.
+		JTextField textfield = new JTextField();
 		
-		JLabel lblNewLabel = new JLabel("  Name:");
-		lblNewLabel.setBounds(33, 146, 62, 18);
-		add(lblNewLabel);
+		name = new JTextField();
+		name.setBounds(101, 142, 150, 24);
+		frame.getContentPane().add(name);
+		name.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("  ID :");
-		lblNewLabel_1.setBounds(45, 187, 34, 18);
-		add(lblNewLabel_1);
+		id = new JTextField();
+		id.setBounds(101, 190, 150, 24);
+		frame.getContentPane().add(id);
+		id.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("PWS:");
-		lblNewLabel_2.setBounds(45, 230, 50, 18);
-		add(lblNewLabel_2);
+		pw = new JTextField();
+		pw.setBounds(101, 237, 150, 24);
+		frame.getContentPane().add(pw);
+		pw.setColumns(10);
 		
-		JLabel lblNewLabel_3 = new JLabel("Age :");
-		lblNewLabel_3.setBounds(50, 273, 50, 18);
-		add(lblNewLabel_3);
+		age = new JTextField();
+		age.setBounds(101, 282, 150, 24);
+		frame.getContentPane().add(age);
+		age.setColumns(10);
 		
-		JLabel lblNewLabel_4 = new JLabel("성별 (M / F)");
-		lblNewLabel_4.setBounds(121, 320, 93, 18);
-		add(lblNewLabel_4);
+		Height = new JTextField();
+		Height.setBounds(101, 412, 150, 24);
+		frame.getContentPane().add(Height);
+		Height.setColumns(10);
 		
-		textField = new JTextField();
-		textField.setBounds(101, 144, 131, 24);
-		add(textField);
-		textField.setColumns(10);
+		Weight = new JTextField();
+		Weight.setBounds(101, 475, 150, 24);
+		frame.getContentPane().add(Weight);
+		Weight.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(101, 185, 131, 24);
-		add(textField_1);
-		textField_1.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(101, 228, 131, 24);
-		add(textField_2);
-		textField_2.setColumns(10);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(101, 270, 131, 24);
-		add(textField_3);
-		textField_3.setColumns(10);
-		
-		textField_4 = new JTextField();
-		textField_4.setBounds(101, 425, 131, 24);
-		add(textField_4);
-		textField_4.setColumns(10);
-		
-		textField_5 = new JTextField();
-		textField_5.setBounds(101, 477, 131, 24);
-		add(textField_5);
-		textField_5.setColumns(10);
 		
 		JLabel lblNewLabel_7 = new JLabel("회원 정보를 입력하세요");
 		lblNewLabel_7.setFont(new Font("굴림", Font.PLAIN, 18));
 		lblNewLabel_7.setBounds(79, 74, 209, 36);
-		add(lblNewLabel_7);
+		frame.getContentPane().add(lblNewLabel_7);
 		
-		JButton btnNewButton_2 = new JButton("확인");
-		btnNewButton_2.setBounds(121, 513, 93, 27);
-		add(btnNewButton_2);
+		JCheckBox chckbxNewCheckBox_M = new JCheckBox("남자");
+		chckbxNewCheckBox_M.setBounds(103, 343, 56, 23);
+		frame.getContentPane().add(chckbxNewCheckBox_M);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("남(M)");
-		chckbxNewCheckBox.setBounds(94, 369, 73, 27);
-		add(chckbxNewCheckBox);
+		JCheckBox chckbxNewCheckBox_F = new JCheckBox("여자");
+		chckbxNewCheckBox_F.setBounds(187, 343, 64, 23);
+		frame.getContentPane().add(chckbxNewCheckBox_F);
 		
-		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("여(F)");
-		chckbxNewCheckBox_1.setBounds(191, 369, 82, 27);
-		add(chckbxNewCheckBox_1);
+		txty = new JTextField();
+		txty.setText("성 별 (Y / N)");
+		txty.setBounds(111, 316, 116, 21);
+		frame.getContentPane().add(txty);
+		txty.setColumns(10);
 		
-		JButton btnNewButton = new JButton("중복확인");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnNewButton_1 = new JButton("가입 완료");
+		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
+				frame.getContentPane().add(textfield, BorderLayout.CENTER);
+				
+				
+				
+				System.out.println("완료되었습니다 ");
 			}
 		});
+		btnNewButton_1.setBounds(130, 511, 97, 23);
+		frame.getContentPane().add(btnNewButton_1);
 		
-		btnNewButton.setBounds(244, 183, 89, 27);
-		add(btnNewButton);
+		btnNewButton = new JButton("중복확인");
+		btnNewButton.setBounds(248, 189, 90, 24);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
+class ImagePanel extends JPanel{
+	private Image img;
+	
+	public ImagePanel(Image img) {
+		this.img = img;
+		
+		setSize(new Dimension(img.getWidth(null),img.getHeight(null)));
+		
+		setPreferredSize(new Dimension(img.getWidth(null),img.getHeight(null)));
+		setLayout(null);
+	}
+	public void paintComponent(Graphics g) {
+		g.drawImage(img,0,0,null);
 	}
 }
 
--- miniProject/src/com/kh/view/login/LoginMainView.java
+++ miniProject/src/com/kh/view/login/LoginMainView.java
@@ -1,15 +1,12 @@
-package com.kh.view.login;
+package 운동관리프로그램.view.login;
 
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -20,18 +17,11 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import com.kh.model.vo.User;
-import com.kh.view.main.Main;
-import javax.swing.SwingConstants;
+public class LoginMainView {
 
-public class LoginMainView extends JPanel {
-
-	private JTextField textField;
+	private JFrame frame;
 	private JPasswordField passwordField;
-
-	private User user;
-
-	private Main main;
+	private JPasswordField passwordField_1;
 
 	/**
 	 * Launch the application.
@@ -40,12 +30,8 @@ public class LoginMainView extends JPanel {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					JFrame window = new JFrame();
-					LoginMainView loginView = new LoginMainView();
-					window.setBounds(100, 100, 360, 600);
-					window.getContentPane().add(loginView);
-					window.setVisible(true);
-					window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					LoginMainView window = new LoginMainView();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -58,118 +44,88 @@ public class LoginMainView extends JPanel {
 	 */
 	public LoginMainView() {
 		initialize();
-
-	}
-
-	public LoginMainView(User user) {
-		this();
-		this.user = user;
-		
-		this.user = new User("test", "1234", "김태훈", 20, 100, 100, '남', false);
-	}
-
-
-	public LoginMainView(Main main, User user) {
-		this(user);
-		this.main = main;
-
-		updateUser();
-	}
-
-	// login 성공시 해당 user를 다른 part의 넣어 패널을 생성하기 위한 메소드
-	public void updateUser() {
-		main.lazeInitPanel(user);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-
-		setBounds(0, 0, 360, 600);
-		setLayout(null);
-
+		frame = new JFrame();
+		frame.setBounds(100, 100, 360, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(true);
+		frame.getContentPane().setLayout(null);
+		
+		ImagePanel panel = new ImagePanel(new ImageIcon("./image/LoginImage.jpg").getImage());
+		frame.getContentPane().add(panel);
+	
+		
+		
+		
 		JButton btnNewButton = new JButton("확인");
 		btnNewButton.setFont(new Font("Arial Unicode MS", Font.PLAIN, 15));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-
-				String id = "admin";
-				String pass = "1234";
-
-				if (id.equals(textField.getText()) && pass.equals(passwordField.getText())) {
-					JOptionPane.showMessageDialog(null, "인증되었습니다");
-					// 로그인 성공이 되었을때
-
-					// 메인 페이지로 이동
-				} else {
-					JOptionPane.showConfirmDialog(null, "다시 확인하십시오");
+				
+					String id = "admin";
+					String pass = "1234";
+					
+					if(id.equals(textField.getText())&& pass.equals(passwordField.getText())) {
+						JOptionPane.showMessageDialog(null, "인증되었습니다");
+						//로그인 성공이 되었을때 
+						
+						
+						//메인 페이지로 이동
+					}else {
+						JOptionPane.showConfirmDialog(null, "다시 확인하십시오");
+					}
+					
 				}
-
-			}
-
+			
 		});
-		btnNewButton.setBounds(146, 296, 70, 34);
-		add(btnNewButton);
-
-		textField = new JTextField();
-		textField.setBounds(136, 216, 116, 24);
-		add(textField);
-		textField.setColumns(10);
-
+		btnNewButton.setBounds(129, 334, 70, 34);
+		frame.getContentPane().add(btnNewButton);
+		
 		JLabel lblNewLabel = new JLabel("Login : ");
-		lblNewLabel.setBounds(86, 204, 166, 49);
-		add(lblNewLabel);
-
+		lblNewLabel.setBounds(56, 256, 51, 24);
+		frame.getContentPane().add(lblNewLabel);
+		
 		JLabel lblNewLabel_1 = new JLabel("PW : ");
 		JPasswordField txtPass = new JPasswordField(10);
-		lblNewLabel_1.setBounds(97, 254, 62, 18);
-		add(lblNewLabel_1);
-
+		lblNewLabel_1.setBounds(68, 293, 37, 18);
+		frame.getContentPane().add(lblNewLabel_1);
+		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(136, 251, 116, 24);
-		add(passwordField);
-
+		passwordField.setBounds(128, 287, 127, 35);
+		frame.getContentPane().add(passwordField);
+		
 		JButton btnNewButton_1 = new JButton("회원가입");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-
+				
 			}
 		});
 		btnNewButton_1.setBounds(111, 514, 105, 27);
-		add(btnNewButton_1);
+		frame.getContentPane().add(btnNewButton_1);
 		
-		JLabel lblHome = new JLabel("Home");
-		lblHome.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHome.setBounds(150, 561, 60, 29);
-		lblHome.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("메인 페이지로 이동");
-				main.convertPanel("main");
-			}
-		});
-		add(lblHome);
-		
-				ImagePanel panel = new ImagePanel(new ImageIcon("./image/LoginPage.jpg").getImage());
-				panel.setSize(getSize());
-				add(panel);
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(128, 248, 127, 35);
+		frame.getContentPane().add(passwordField_1);
 	}
 }
-
-class ImagePanel extends JPanel {
+class ImagePanel extends JPanel{
 	private Image img;
-
+	
 	public ImagePanel(Image img) {
 		this.img = img;
-
-		setSize(new Dimension(img.getWidth(null), img.getHeight(null)));
-
-		setPreferredSize(new Dimension(img.getWidth(null), img.getHeight(null)));
+		
+		setSize(new Dimension(img.getWidth(null),img.getHeight(null)));
+		
+		setPreferredSize(new Dimension(img.getWidth(null),img.getHeight(null)));
 		setLayout(null);
 	}
-
 	public void paintComponent(Graphics g) {
-		g.drawImage(img, 0, 0, null);
+		g.drawImage(img,0,0,null);
 	}
 }
