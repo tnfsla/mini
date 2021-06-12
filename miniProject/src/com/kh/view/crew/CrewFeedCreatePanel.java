587f63c965f14642a6c606bb717eae91c962b960
kthss01 <kthss01@naver.com>
2021-06-13 05:01:06 +0900

    Crew Part Feed Feed 메인 페이지의 List 패널 이미지 작업

--- miniProject/src/com/kh/view/crew/CrewFeedCreatePanel.java
+++ miniProject/src/com/kh/view/crew/CrewFeedCreatePanel.java
@@ -39,7 +39,6 @@ public class CrewFeedCreatePanel extends JPanel {
 
 		JPanel panelFeedBack = new JPanel();
 		panelFeedBack.setBounds(15, 10, 30, 30);
-		panelFeedBack.add(new CrewImagePanel("./images/back.png", panelFeedBack.getSize()));
 		add(panelFeedBack);
 		panelFeedBack.setLayout(null);
 
@@ -54,6 +53,8 @@ public class CrewFeedCreatePanel extends JPanel {
 		btnFeedBack.setBorderPainted(false);
 		btnFeedBack.setBounds(0, 0, 30, 30);
 		panelFeedBack.add(btnFeedBack);
+		CrewImagePanel crewImagePanelFeedBack = new CrewImagePanel("./images/back.png", panelFeedBack.getSize());
+		panelFeedBack.add(crewImagePanelFeedBack);
 
 		JLabel lblFeedCreate = new JLabel("글쓰기");
 		lblFeedCreate.setFont(new Font(CrewViewManager.MAIN_FONT, Font.BOLD, 18));
@@ -72,8 +73,8 @@ public class CrewFeedCreatePanel extends JPanel {
 		btnCreateFeed.setFont(new Font(CrewViewManager.MAIN_FONT, Font.PLAIN, 12));
 		btnCreateFeed.setContentAreaFilled(false);
 		btnCreateFeed.setBorderPainted(false);
-		CrewImagePanel crewImagePanel = new CrewImagePanel("./images/crew_feed_button.png", panelCreateFeed.getSize());
-		panelCreateFeed.add(crewImagePanel);
+		CrewImagePanel crewImagePanelCreateFeed = new CrewImagePanel("./images/crew_feed_button.png", panelCreateFeed.getSize());
+		panelCreateFeed.add(crewImagePanelCreateFeed);
 
 		btnCreateFeed.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -89,7 +90,6 @@ public class CrewFeedCreatePanel extends JPanel {
 
 		JPanel panelFeedTitle = new JPanel();
 		panelFeedTitle.setBounds(12, 50, 336, 50);
-		panelFeedTitle.add(new CrewImagePanel("./images/crew_create_crewname_edit.png", panelFeedTitle.getSize()));
 		add(panelFeedTitle);
 		panelFeedTitle.setLayout(null);
 
@@ -99,6 +99,8 @@ public class CrewFeedCreatePanel extends JPanel {
 		panelFeedTitle.add(textFieldFeedTitle);
 		textFieldFeedTitle.setText("제목을 입력해주세요");
 		textFieldFeedTitle.setColumns(10);
+		CrewImagePanel crewImagePanelFeedTitle = new CrewImagePanel("./images/crew_create_crewname_edit.png", panelFeedTitle.getSize());
+		panelFeedTitle.add(crewImagePanelFeedTitle);
 
 		JPanel panelFeedContents = new JPanel();
 		panelFeedContents.setBackground(CrewViewManager.COLOR_MINT);
