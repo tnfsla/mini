7fc6dfb1b9f43bb2e81ec9e2be33cba1050837f2
kthss01 <kthss01@naver.com>
2021-06-14 19:15:38 +0900

    Crew Part Feed 작성시 글 내용 안나오는 버그 수정

--- miniProject/src/com/kh/model/vo/Feed.java
+++ miniProject/src/com/kh/model/vo/Feed.java
@@ -6,12 +6,14 @@ import java.util.Calendar;
 public class Feed extends Message {
 
 	private String title; // 피드 제목
+	private String contents; // 피드 내용
 	private ArrayList<String> likeUserList; // 좋아요 한 유저 이름 리스트
 	private ArrayList<Comment> commentList; // 댓글 리스트
 
 	public Feed(String userId, Calendar date, String title, String contents) {
 		super(userId, date, title);
 		this.title = title;
+		this.contents = contents;
 
 		commentList = new ArrayList<Comment>();
 		likeUserList = new ArrayList<String>();
@@ -23,6 +25,14 @@ public class Feed extends Message {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
 	}
 
 	public ArrayList<Comment> getCommentList() {
